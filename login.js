f27597f3daad8d79e69722e20cec5ba60db5074a
Umesh Dhauni <malkeet@kilobytetech.com>
2020-09-07 21:36:20 +0530

    Modal

---
+++ login.js
@@ -0,0 +1,11 @@
+// var pass = document.getElementById("pwd1")
+// var confPass = document.getElementById("pwd2")
+
+function myFunction() {
+    var element = document.getElementById("modal-login-hidden");
+    element.classList.add("modal-login-show");
+    
+    var element2 = document.getElementById("main-body");
+    element2.classList.add("body-change");
+ }
+
